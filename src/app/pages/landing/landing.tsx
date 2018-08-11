3e5fc95b614c70feff49d609295e51dec84f6191
Krisboi Tibi <krisboi.tibi@gmail.com>
2018-08-11 22:22:25 +0300

    landing page layout

--- src/app/pages/landing/landing.tsx
+++ src/app/pages/landing/landing.tsx
@@ -1,11 +1,14 @@
 import { h, Component } from 'preact';
+import { route } from 'preact-router';
 
 import TopAppBar from "preact-material-components/TopAppBar";
 import Icon from "preact-material-components/Icon";
 import LayoutGrid from "preact-material-components/LayoutGrid";
+import Button from 'preact-material-components/Button';
 
 import "./landing.scss";
 
+//TODO: move top app bar in a layout component
 export class LandingPage extends Component {
     render() {
         return (
@@ -13,18 +16,34 @@ export class LandingPage extends Component {
                 <TopAppBar fixed>
                     <TopAppBar.Row>
                         <TopAppBar.Section align-start>
-                            <TopAppBar.Icon>sentiment_satisfied_alt</TopAppBar.Icon>
+                            <img class="top-appbar-icon" src="/assets/logo.svg"></img>
                         </TopAppBar.Section>
                     </TopAppBar.Row>
                 </TopAppBar>
 
                 <LayoutGrid className="mdc-top-app-bar--fixed-adjust">
                     <LayoutGrid.Inner>
-                        <LayoutGrid.Cell align="middle" cols={6}>
-                            <div style="width: 100%, height: 50px; background: red">asdasdas</div>
+                        <LayoutGrid.Cell cols={12} className="center">
+                            <h1 class="title mdc-typography--headline2">Moonlet</h1>
+                            <Icon className="icon">account_balance_wallet</Icon>
                         </LayoutGrid.Cell>
-                        <LayoutGrid.Cell align="middle" cols={6}>
-                            <div style="width: 100%, height: 50px; background: red">asdasdas</div>
+                        <LayoutGrid.Cell cols={12} className="center">
+                            <Button 
+                                ripple secondary raised 
+                                className="create-wallet"
+                                onClick={() => route('/create-wallet')}
+                            >
+                                Create new wallet
+                            </Button>
+                        </LayoutGrid.Cell>
+                        <LayoutGrid.Cell cols={12} className="center">
+                            <Button 
+                                ripple raised 
+                                className="restore-wallet"
+                                onClick={() => route('import-wallet')}
+                            >
+                                Restore existing wallet
+                            </Button>
                         </LayoutGrid.Cell>
                     </LayoutGrid.Inner>
                 </LayoutGrid>
