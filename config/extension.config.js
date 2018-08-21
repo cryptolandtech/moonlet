d0d73f9c0b6288d5d130e1f6eec26c487c84725e
Krisboi Tibi <krisboi.tibi@gmail.com>
2018-08-21 23:58:38 +0300

    update extension config

--- config/extension.config.js
+++ config/extension.config.js
@@ -2,7 +2,7 @@ import common from "./common.config";
 import {resolve} from "path";
 
 import WriteFilePlugin from "write-file-webpack-plugin";
-import CopyWebpackPlugin from 'copy-webpack-plugin';
+import CopyWebpackPlugin from "copy-webpack-plugin";
 /**
  * Function that mutates original webpack config.
  * Supports asynchronous changes when promise is returned.
@@ -13,20 +13,20 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
  **/
 export default function (config, env, helpers) {
     // apply common config
-    config.output.publicPath = './';
+    config.output.publicPath = "./";
     common(config, env, helpers);    
 
     // customize config
-    config.resolve.alias['preact-cli-entrypoint'] = resolve(process.cwd(), 'src', 'extension', 'index');
+    config.resolve.alias["preact-cli-entrypoint"] = resolve(process.cwd(), "src", "extension", "index");
 
     // add background script
     config.output.filename = "[name].js";
-    config.entry["background"] = resolve(process.cwd(), 'src', 'extension', 'background');
+    config.entry["background"] = resolve(process.cwd(), "src", "extension", "background");
 
     // overwrite manifest.json 
     config.plugins.unshift(new CopyWebpackPlugin([
         {
-            from: resolve(process.cwd(), 'src', 'extension', 'manifest.json'),
+            from: resolve(process.cwd(), "src", "extension", "manifest.json"),
             to: "manifest.json"
         }
     ]));
@@ -34,12 +34,20 @@ export default function (config, env, helpers) {
     // change eval dev tools to make chrome happy
     config.devtool = "inline-source-map";
 
-    // make hot reload work 
-    config.entry.bundle = [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/dev-server',
-        config.entry.bundle[0]
-    ];
+    //make hot reload work 
+    if (Array.isArray(config.entry.bundle)) {
+        config.entry.bundle = config.entry.bundle.map((bundle) => {
+            if (bundle === "webpack-dev-server/clientasd") {
+                const liveReloadProtocol = env.https ? "https" : "http";
+                const liveReloadHost = env.host.replace(/^0\.0\.0\.0$/, "localhost");
+
+                const liveReloadUrl = `${liveReloadProtocol}://${liveReloadHost}:${env.port}`;
+                return "webpack-dev-server/client?" + liveReloadUrl;
+            }
+
+            return bundle;
+        });
+    }
 
     // used for dev to load component in browser
     config.plugins.push(
