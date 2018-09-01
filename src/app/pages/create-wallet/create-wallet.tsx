9d616ccad434be22a23f202bca65898e18f8172a
narcis <narcis@creamcapital.io>
2018-09-01 11:44:01 +0300

    initial work

--- src/app/pages/create-wallet/create-wallet.tsx
+++ src/app/pages/create-wallet/create-wallet.tsx
@@ -1,10 +1,18 @@
-import { h, Component } from 'preact';
+import {h, Component} from 'preact';
+
+import {TopBar} from "../layouts/top-bar/top-bar";
+import LayoutGrid from "preact-material-components/LayoutGrid";
+import "preact-material-components/Card/style.css";
+import "./create-wallet.scss";
 
 export class CreateWalletPage extends Component {
-    render() {
+    render(props, state) {
         return (
-            <div>
-                ---- CreateWalletPage ----
+            <div className="create-wallet-page">
+                <TopBar title="Create New Wallet"/>
+                <LayoutGrid className="mdc-top-app-bar--fixed-adjust">
+                        <span class="mdc-top-app-bar__title">Content</span>
+                </LayoutGrid>
             </div>
         );
     }
