cdb22c062c8bdcbfa67134dd9c887d455194a54d
Krisboi Tibi <krisboi.tibi@gmail.com>
2018-09-02 03:52:38 +0300

    setup precommit hook

--- src/app/utils/merge-deep.ts
+++ src/app/utils/merge-deep.ts
@@ -8,7 +8,7 @@ export const mergeDeep = <T extends object = object>(target: T, ...sources: T[])
   }
 
   if (isMergebleObject(target) && isMergebleObject(source)) {
-    Object.keys(source).forEach(function(key: string) {
+    Object.keys(source).forEach((key: string) => {
       if (isMergebleObject(source[key])) {
         if (!target[key]) {
           target[key] = {};
@@ -30,56 +30,3 @@ const isObject = (item: any): boolean => {
 const isMergebleObject = (item): boolean => {
   return isObject(item) && !Array.isArray(item);
 };
-
-/**
-   * describe('merge', () => {
-  it('should merge Objects and all nested Ones', () => {
-    const obj1 = { a: { a1: 'A1'}, c: 'C', d: {} };
-    const obj2 = { a: { a2: 'A2'}, b: { b1: 'B1'}, d: null };
-    const obj3 = { a: { a1: 'A1', a2: 'A2'}, b: { b1: 'B1'}, c: 'C', d: null};
-    expect(mergeObjects({}, obj1, obj2)).toEqual(obj3);
-  });
-  it('should behave like Object.assign on the top level', () => {
-    const obj1 = { a: { a1: 'A1'}, c: 'C'};
-    const obj2 = { a: undefined, b: { b1: 'B1'}};
-    expect(mergeObjects({}, obj1, obj2)).toEqual(Object.assign({}, obj1, obj2));
-  });
-  it('should not merge array values, just override', () => {
-    const obj1 = {a: ['A', 'B']};
-    const obj2 = {a: ['C'], b: ['D']};
-    expect(mergeObjects({}, obj1, obj2)).toEqual({a: ['C'], b: ['D']});
-  });
-  it('typed merge', () => {
-    expect(mergeObjects<TestPosition>(new TestPosition(0, 0), new TestPosition(1, 1)))
-      .toEqual(new TestPosition(1, 1));
-  });
-});
-
-class TestPosition {
-  constructor(public x: number = 0, public y: number = 0) {\/*empty*\describe('merge', () => {
-  it('should merge Objects and all nested Ones', () => {
-    const obj1 = { a: { a1: 'A1'}, c: 'C', d: {} };
-    const obj2 = { a: { a2: 'A2'}, b: { b1: 'B1'}, d: null };
-    const obj3 = { a: { a1: 'A1', a2: 'A2'}, b: { b1: 'B1'}, c: 'C', d: null};
-    expect(mergeObjects({}, obj1, obj2)).toEqual(obj3);
-  });
-  it('should behave like Object.assign on the top level', () => {
-    const obj1 = { a: { a1: 'A1'}, c: 'C'};
-    const obj2 = { a: undefined, b: { b1: 'B1'}};
-    expect(mergeObjects({}, obj1, obj2)).toEqual(Object.assign({}, obj1, obj2));
-  });
-  it('should not merge array values, just override', () => {
-    const obj1 = {a: ['A', 'B']};
-    const obj2 = {a: ['C'], b: ['D']};
-    expect(mergeObjects({}, obj1, obj2)).toEqual({a: ['C'], b: ['D']});
-  });
-  it('typed merge', () => {
-    expect(mergeObjects<TestPosition>(new TestPosition(0, 0), new TestPosition(1, 1)))
-      .toEqual(new TestPosition(1, 1));
-  });
-});
-
-class TestPosition {
-  constructor(public x: number = 0, public y: number = 0) {\/*empty*\/}
-}
-   */
--- src/extension/background.ts
+++ src/extension/background.ts
@@ -1,1 +1,1 @@
-console.log('BG');
+// console.error('BG');
