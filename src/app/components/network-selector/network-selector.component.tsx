123485cf448109871f30b70247b08439c4daf472
Krisboi Tibi <krisboi.tibi@gmail.com>
2018-09-09 03:21:05 +0300

    ugly hack to fix types errors

--- src/app/components/network-selector/network-selector.component.tsx
+++ src/app/components/network-selector/network-selector.component.tsx
@@ -4,14 +4,19 @@ import Chips from 'preact-material-components/Chips';
 import './network-selector.scss';
 
 export class NetworkSelector extends Component {
+  public removeType(content): any {
+    return content;
+  }
   public render() {
     return (
       <div class="network-selector">
         <Chips>
           <Chips.Chip>
-            <Chips.Text>
-              <div class="circle" />
-            </Chips.Text>
+            {this.removeType(
+              <Chips.Text>
+                <div class="circle" />
+              </Chips.Text>
+            )}
             <Chips.Text>Main Zilliqa Network</Chips.Text>
             <Chips.Icon>keyboard_arrow_down</Chips.Icon>
           </Chips.Chip>
