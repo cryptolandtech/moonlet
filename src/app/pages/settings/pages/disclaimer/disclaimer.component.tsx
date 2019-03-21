20417a83c7b1049baa599dc56246f15f7de814a0
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-03-21 03:36:42 +0200

    add disclaimer page content

--- src/app/pages/settings/pages/disclaimer/disclaimer.component.tsx
+++ src/app/pages/settings/pages/disclaimer/disclaimer.component.tsx
@@ -1,7 +1,35 @@
 import { h, Component } from 'preact';
+import { Translate } from '../../../../components/translate/translate.component';
+
+import './disclaimer.scss';
 
 export class DisclaimerPage extends Component {
     public render() {
-        return <div class="disclaimer-page" />;
+        return (
+            <div class="disclaimer-page">
+                <p class="strong-text">
+                    <Translate text="DisclaimerPage.p1" />
+                </p>
+                <p>
+                    <Translate text="DisclaimerPage.p21" />
+                    <Translate className="strong-text" text="DisclaimerPage.p22" />
+                </p>
+                <p>
+                    <Translate className="strong-text" text="DisclaimerPage.p31" />
+                    <Translate text="DisclaimerPage.p32" />
+                </p>
+                <p>
+                    <Translate className="strong-text" text="DisclaimerPage.p41" />
+                    <br />
+                    <Translate text="DisclaimerPage.p42" />
+                    <br />
+                    <Translate text="DisclaimerPage.p43" />
+                    <br />
+                    <Translate text="DisclaimerPage.p44" />
+                    <br />
+                    <Translate className="strong-text" text="DisclaimerPage.p45" />
+                </p>
+            </div>
+        );
     }
 }
