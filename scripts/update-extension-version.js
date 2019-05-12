37e655cc5fd44f432e67bcb3d2c6c7e2ebf09a45
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-05-13 02:05:03 +0300

    fix build script

--- scripts/update-extension-version.js
+++ scripts/update-extension-version.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const resolve = require('path').resolve;
-const manifest = require('../src/extension/manifest.json');
+const manifest = require('../src/platforms/extension/manifest.json');
 const packageJson = require('../package.json');
 
 manifest.version = packageJson.version;
 
 fs.writeFileSync(
-    resolve(__dirname, '../src/extension/manifest.json'),
+    resolve(__dirname, '../src/platforms/extension/manifest.json'),
     JSON.stringify(manifest, null, 4)
 );
