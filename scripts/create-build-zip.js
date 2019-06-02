8d8302fe23b45ae7b0b70b3e41b1b45771c4ff69
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-06-03 02:41:21 +0300

    improve extension build file name

--- scripts/create-build-zip.js
+++ scripts/create-build-zip.js
@@ -1,8 +1,9 @@
 const zipFolder = require('zip-folder');
 const resolve = require('path').resolve;
+const version = require('../build/extension/manifest.json').version;
 
 const SOURCE_FOLDER = resolve(__dirname, '../build/extension');
-const ZIP_FILE_NAME = resolve(__dirname, '../build/extension.zip');
+const ZIP_FILE_NAME = resolve(__dirname, `../build/chrome-extension-${version}.zip`);
 
 zipFolder(SOURCE_FOLDER, ZIP_FILE_NAME, function(err) {
     if (err) {
