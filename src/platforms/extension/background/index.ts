08ea3ac15e7f1de9698fbdbd1927fb9544cc3f24
Krisboi Tibi <krisboi.tibi@gmail.com>
2019-06-25 23:53:15 +0300

    update version for popup display

--- src/platforms/extension/background/index.ts
+++ src/platforms/extension/background/index.ts
@@ -129,7 +129,7 @@ browser.runtime.onInstalled.addListener(async (details: Runtime.OnInstalledDetai
     if (
         details.reason === 'update' &&
         details.previousVersion !== version &&
-        ['0.28.0'].indexOf(version) >= 0
+        ['0.29.0'].indexOf(version) >= 0
     ) {
         browser.tabs.create({
             url: 'https://moonlet.xyz/thank-you-update/'
